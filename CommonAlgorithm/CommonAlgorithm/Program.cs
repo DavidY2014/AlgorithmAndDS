7455c6d729eaf5254b73ef34a735e92d6ce680cd
david <wu.xiguanghua@163.com>
2020-03-21 20:49:16 +0800

    init

---
+++ CommonAlgorithm/CommonAlgorithm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommonAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
